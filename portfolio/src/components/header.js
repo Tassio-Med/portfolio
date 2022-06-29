a60854f06a13f7a17495a9ed3f07c5c2ecfca531
Tassio Med <tassiohfmed@gmail.com>
2022-06-29 17:45:10 -0300

    alinhando os elementos

--- portfolio/src/components/header.js
+++ portfolio/src/components/header.js
@@ -3,17 +3,19 @@ import Hand from '../gif/peace1.gif'
 
 function Header(){
   return(
-    <div>
+    <div className="header">
       <nav>
-        <li className="projects">
-          <a href="#projects">Meus projetos</a>
+        <div class="elem-navigation">
+          <li className="projects">
+            <a href="#projects">Meus projetos</a>
+            </li>
+          <li className="about">
+            <a href="#about">Quem sou eu?</a>
           </li>
-        <li className="about">
-          <a href="#about">Quem sou eu?</a>
-        </li>
-        <li className="contact">
-          <a href="#contact">Fale comigo</a>
-        </li>
+          <li className="contact">
+            <a href="#contact">Fale comigo</a>
+          </li>
+        </div>
       </nav>
       <h1>Bem-vindo ao meu Portfólio</h1>
       <div className="bckggif">
