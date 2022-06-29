9013ba9de17669aeb05cb51758ede3715b0c8120
Tassio Med <tassiohfmed@gmail.com>
2022-06-29 20:34:09 -0300

    Engatinhando no alinhamento do 'bem-vindo'

--- portfolio/src/components/about.js
+++ portfolio/src/components/about.js
@@ -2,7 +2,7 @@ import './about.css';
 
 function About(){
   return(
-    <div>
+    <div id="about">
       <h2 className="abouttitle">Sobre mim</h2>
       <p>
         Sed eu felis a magna hendrerit eleifend vitae non massa. Praesent commodo pretium augue. Quisque imperdiet sagittis neque, vitae scelerisque nulla porta in. Phasellus quis euismod risus. Praesent ut eros convallis, auctor nunc quis, luctus dui. Nullam sem lorem, blandit ut leo et, cursus ultricies ipsum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In hac habitasse platea dictumst.Sed eu felis a magna hendrerit eleifend vitae non massa. Praesent commodo pretium augue. Quisque imperdiet sagittis neque, vitae scelerisque nulla porta in. Phasellus quis euismod risus. Praesent ut eros convallis, auctor nunc quis, luctus dui. Nullam sem lorem, blandit ut leo et, cursus ultricies ipsum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In hac habitasse platea dictumst.
