2ae46ed75f1edef04bcdcba65cf4667926febe77
Tassio Med <tassiohfmed@gmail.com>
2022-07-04 17:24:38 -0300

    adicionando imagens nos cards

--- portfolio/src/components/projects.js
+++ portfolio/src/components/projects.js
@@ -1,16 +1,18 @@
 import './projects.css';
+import Image from '../images/todolist.jpg';
 
 function Projects(){
   return(
     <div id="projects">
       <ul className="projects">
-        <li className="project1 grow-card">
+        <li className="project1 grow-card pro1-img">
+          <img src={ Image } alt="todolist" className="img-todo"></img>
           <h3 >Projeto 1</h3>
         </li>
-        <li className="project2 grow-card">
+        <li className="project2 grow-card pro2-img">
           <h3>Projeto 2</h3>
         </li>
-        <li className="project3 grow-card">
+        <li className="project3 grow-card pro1-img">
           <h3>Projeto 3</h3>
         </li>
       </ul>
